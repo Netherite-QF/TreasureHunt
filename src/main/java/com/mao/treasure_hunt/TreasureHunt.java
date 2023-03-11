ab7ebf456426514353f1a0c27e3755002759e92c
Oganesson897 <101081378+Darknight123MC@users.noreply.github.com>
2023-03-11 11:16:41 +0900

    Update TreasureHunt.java

--- src/main/java/com/mao/treasure_hunt/TreasureHunt.java
+++ src/main/java/com/mao/treasure_hunt/TreasureHunt.java
@@ -4,6 +4,7 @@ import com.mao.treasure_hunt.init.BlockInit;
 import com.mao.treasure_hunt.init.EnchantmentInit;
 import com.mao.treasure_hunt.init.ItemInit;
 import com.mao.treasure_hunt.init.WorldInit;
+import net.minecraft.util.Identifier;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,10 @@ import org.slf4j.LoggerFactory;
 public class TreasureHunt implements ModInitializer {
     public static final String MODID = "treasure_hunt";
     public static final Logger LOGGER = LoggerFactory.getLogger(TreasureHunt.class);
+    
+    public static Identifier asId(String path) {
+        return new Identifier(MODID, path);
+    }
 
     @Override
     public void onInitialize() {
